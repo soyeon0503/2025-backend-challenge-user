a556e3c44223c23390da94e7d21c30c52d7da20e
soyeon <bless0053@gmail.com>
2025-01-26 12:49:39 +0900

    modify:init.sh

--- init.sh
+++ init.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+cd src
+
+cp .env.example .env
+
+cd ../
+
 docker-compose up -d
 
 docker compose exec app npm install
@@ -9,5 +15,7 @@ docker compose exec app composer install
 docker compose exec app php artisan key:generate
 
 docker compose exec app php artisan migrate --seed
+
+docker compose exec app npm run build
 
 docker compose exec app npm run dev
