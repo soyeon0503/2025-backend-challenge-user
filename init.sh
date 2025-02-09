c93c506c9353a2e10b8bf116b121c21d7e819562
soyeon <bless0053@gmail.com>
2025-02-09 18:28:46 +0900

    fix:dockercompose

--- init.sh
+++ init.sh
@@ -8,23 +8,23 @@ cd ../
 
 docker-compose up -d
 
-docker compose exec app npm install
+docker-compose exec app npm install
 
 docker-compose exec app composer install
 
 docker-compose exec app php artisan key:generate
 
-docker-compose exec app php artisan migrate --seed
+docker-compose exec app php artisan migrate:refresh --seed
 
 
 docker-compose exec app php artisan config:clear 
 
 docker compose exec app php artisan cache:clear
 
-docker compose exec app php artisan route:clear
+docker-compose exec app php artisan route:clear
 
-docker compose exec app php artisan view:clear
+docker-compose exec app php artisan view:clear
 
-docker compose exec app npm run build
+docker-compose exec app npm run build
 
 docker-compose exec app npm run dev
