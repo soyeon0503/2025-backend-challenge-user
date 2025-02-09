fb4e5b45732bf0c77e7cca66656fee99d973876d
soyeon <bless0053@gmail.com>
2025-02-09 16:00:15 +0900

    feature:route追加

--- src/routes/web.php
+++ src/routes/web.php
@@ -1,10 +1,19 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\PostController;
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\UserController;
+use Illuminate\Support\Facades\Auth;
 
+// 인증 관련 라우트
+Auth::routes();
+
+// 로그인 상태에 따라 홈 또는 로그인 페이지로 리다이렉트
 Route::get('/', function () {
-    return redirect()->route('posts.index');
-});
+    return redirect()->route(Auth::check() ? 'users.index' : 'login');
+})->middleware('web');
 
-Route::resource('posts', PostController::class);
+// 로그인한 사용자만 접근 가능한 라우트
+Route::middleware(['auth'])->group(function () {
+    Route::resource('users', UserController::class);
+});
