54dab0c0efad8671e1b043e0a2f355e699a21efe
soyeon <bless0053@gmail.com>
2025-02-09 17:55:45 +0900

    fix:env

--- init.sh
+++ init.sh
@@ -10,14 +10,14 @@ docker-compose up -d
 
 docker compose exec app npm install
 
-docker compose exec app composer install
+docker-compose exec app composer install
 
-docker compose exec app php artisan key:generate
+docker-compose exec app php artisan key:generate
 
-docker compose exec app php artisan migrate --seed
+docker-compose exec app php artisan migrate --seed
 
 
-docker compose exec app php artisan config:clear 
+docker-compose exec app php artisan config:clear 
 
 docker compose exec app php artisan cache:clear
 
@@ -27,4 +27,4 @@ docker compose exec app php artisan view:clear
 
 docker compose exec app npm run build
 
-docker compose exec app npm run dev
+docker-compose exec app npm run dev
